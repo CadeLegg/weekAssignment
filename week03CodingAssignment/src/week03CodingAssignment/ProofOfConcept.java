351c0cde8cac4df5e83bb625b05bea6cd88a014c
CadeLegg <cade.legg@outlook.com>
2024-10-22 17:34:01 -0600

    first commit

---
+++ week03CodingAssignment/src/week03CodingAssignment/ProofOfConcept.java
@@ -0,0 +1,16 @@
+package week03CodingAssignment;
+
+public class ProofOfConcept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		// hello there
+		
+		
+		
+		
+	}
+
+}
